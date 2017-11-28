eccc2710f5dac5f5552d5dc5990799c4512ac106
Ki-Hin <in010210@gmail.com>
2017-11-28 12:26:08 +0900

    키보드 후킹 완료

---
+++ KeyLocker/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KeyLocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.실행ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.중지ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.설정ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // notifyMenuStrip
+            // 
+            this.notifyMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.실행ToolStripMenuItem,
+            this.중지ToolStripMenuItem,
+            this.설정ToolStripMenuItem,
+            this.종료ToolStripMenuItem});
+            this.notifyMenuStrip.Name = "notifyMenuStrip";
+            this.notifyMenuStrip.Size = new System.Drawing.Size(153, 114);
+            // 
+            // 실행ToolStripMenuItem
+            // 
+            this.실행ToolStripMenuItem.Name = "실행ToolStripMenuItem";
+            this.실행ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.실행ToolStripMenuItem.Text = "실행";
+            // 
+            // 중지ToolStripMenuItem
+            // 
+            this.중지ToolStripMenuItem.Name = "중지ToolStripMenuItem";
+            this.중지ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.중지ToolStripMenuItem.Text = "중지";
+            // 
+            // 설정ToolStripMenuItem
+            // 
+            this.설정ToolStripMenuItem.Name = "설정ToolStripMenuItem";
+            this.설정ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.설정ToolStripMenuItem.Text = "설정";
+            this.설정ToolStripMenuItem.Click += new System.EventHandler(this.설정ToolStripMenuItem_Click);
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
+            this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 270);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.notifyMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip notifyMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem 실행ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 중지ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 설정ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+    }
+}
+
